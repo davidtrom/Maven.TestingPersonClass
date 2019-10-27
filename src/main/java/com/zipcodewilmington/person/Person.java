2ef32111205d48b4a8d4938a395721cf8f8e04d4
David Trombello <david@MacBook-Pro-8.localdomain>
2019-10-27 16:05:44 -0400

    new fields created, all tests on them pass

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -1,11 +1,20 @@
 package com.zipcodewilmington.person;
 
+import javax.swing.*;
+import java.util.Date;
+
 /**
  * Created by leon on 2/12/18.
  */
 public class Person {
     private String name;
     private int age;
+    private String hairColor;
+    private String eyeColor;
+    private String gender;
+    private Double shoeSize;
+    private String height;
+
 
     public Person() {
         this.name="";
@@ -26,6 +35,16 @@ public class Person {
         this.age = age;
     }
 
+    public Person(String name, int age, String hairColor, String eyeColor, String gender, Double shoeSize, String height) {
+        this.name = name;
+        this.age = age;
+        this.hairColor = hairColor;
+        this.eyeColor = eyeColor;
+        this.gender = gender;
+        this.shoeSize=shoeSize;
+        this.height = height;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -33,6 +52,38 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+
+    public void setHairColor(String hairColor){
+        this.hairColor = hairColor;
+    }
+
+    public void setEyeColor(String eyeColor){
+        this.eyeColor = eyeColor;
+    }
+
+    public void setGender (String gender){
+        this.gender = gender;
+    }
+
+    public void setShoeSize (Double shoeSize)
+    {
+        this.shoeSize = shoeSize;
+    }
+
+    public void setHeight (String height)
+    {
+        this.height = height;
+    }
+
+    public String getHeight () {return this.height;}
+
+    public String getGender () {return this.gender;}
+
+    public String getHairColor () {return this.hairColor;}
+
+    public String getEyeColor () {return this.eyeColor;}
+
+    public Double getShoeSize ()   {return this.shoeSize;}
 
     public String getName() {
         return this.name;
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -95,4 +95,122 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    /*@Test
+    public void testSetHeight() {
+        // Given
+        Person person = new Person();
+        String expected = "5' 11";
+
+        // When
+        person.setHeight(expected);
+        String actual = person.getName();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }*/
+    @Test
+    public void testConstructorWithNewAttributes() {
+        // Given
+        Integer expectedAge = 37;
+        String expectedName = "David";
+        String expectedHairColor = "brown";
+        String expectedEyeColor = "green";
+        String expectedGender = "male";
+        Double expectedShoeSize = 11.5;
+        String expectedHeight = "5' 11''";
+
+
+        // When
+        Person person = new Person(expectedName, expectedAge, expectedHairColor, expectedEyeColor, expectedGender, expectedShoeSize, expectedHeight);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+        String actualHairColor = person.getHairColor();
+        String actualEyeColor = person.getEyeColor();
+        String actualGender = person.getGender();
+        Double actualShoeSize = person.getShoeSize();
+        String actualHeight = person.getHeight();
+
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedHairColor, actualHairColor);
+        Assert.assertEquals(expectedEyeColor, actualEyeColor);
+        Assert.assertEquals(expectedGender, actualGender);
+        Assert.assertEquals(expectedShoeSize, actualShoeSize);
+        Assert.assertEquals(expectedHeight, actualHeight);
+    }
+
+    @Test
+    public void testSetHairColor() {
+        // Given
+        Person person = new Person();
+        String expected = "brown";
+
+        // When
+        person.setHairColor(expected);
+
+        // Then
+        String actual = person.getHairColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetEyeColor() {
+        // Given
+        Person person = new Person();
+        String expected = "green";
+
+        // When
+        person.setEyeColor(expected);
+
+        // Then
+        String actual = person.getEyeColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetGender() {
+        // Given
+        Person person = new Person();
+        String expected = "male";
+
+        // When
+        person.setGender(expected);
+
+        // Then
+        String actual = person.getGender();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetShoeSize() {
+        // Given
+        Person person = new Person();
+        Double expected = 11.5;
+
+        // When
+        person.setShoeSize(expected);
+
+        // Then
+        Double actual = person.getShoeSize();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetHeight() {
+        // Given
+        Person person = new Person();
+        String  expected = "5' 11''";
+
+        // When
+        person.setHeight(expected);
+
+        // Then
+        String actual = person.getHeight();
+        Assert.assertEquals(expected, actual);
+    }
+
 }
